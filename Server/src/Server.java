934dd2b3945d3f0fd515f4331397e7fb981c46d5
crazykushu <34454011+crazykushu@users.noreply.github.com>
2021-04-13 22:13:56 -0700

    backend added
    need to fix jar file import issue

---
+++ Server/src/Server.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Server extends Thread {
+    private final int serverPort;
+
+    private ArrayList<ServerHelper> workerList = new ArrayList<>();
+
+    public Server(int serverPort) {
+        this.serverPort = serverPort;
+    }
+
+    public List<ServerHelper> getWorkerList() {
+        return workerList;
+    }
+
+    @Override
+    public void run() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(serverPort);
+            while(true) {
+                System.out.println("About to accept client connection...");
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Accepted connection from " + clientSocket);
+                ServerHelper worker = new ServerHelper(this, clientSocket);
+                workerList.add(worker);
+                worker.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void removeWorker(ServerHelper serverWorker) {
+        workerList.remove(serverWorker);
+    }
+}
